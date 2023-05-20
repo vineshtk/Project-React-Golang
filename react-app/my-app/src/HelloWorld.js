9e56d75e9ab2ac50cfa941221db3bd80d66596ac
Vinesh tk <vineshtk10@gmail.com>
2023-05-21 00:12:45 +0530

    using functional component

---
+++ react-app/my-app/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+
+function HelloWorld() {
+    return (
+<Fragment>
+    <hr />
+    <h1>Hello, World! from another</h1>
+    
+</Fragment>
+    );
+       
+}
+
+export default HelloWorld;
--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppClass from './AppClass';
+import HelloWorld from './HelloWorld';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppClass />
+    <HelloWorld />
   </React.StrictMode>
 );
 
