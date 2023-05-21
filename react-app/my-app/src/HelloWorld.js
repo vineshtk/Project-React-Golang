c3c8c567df50b02f790a1dcc563aadb011024c7e
Vinesh tk <vineshtk10@gmail.com>
2023-05-21 13:21:57 +0530

    using react state

--- react-app/my-app/src/HelloWorld.js
+++ react-app/my-app/src/HelloWorld.js
@@ -1,11 +1,36 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import "./HelloWorld.css"
 
 function HelloWorld(props) {
+    const [isTrue, setIsTrue]= useState(true)
+
+    const toggleTrue = ()=> {
+        if (isTrue){
+            setIsTrue(false)
+            return
+        }
+        setIsTrue(true)
+    }
     return (
         <Fragment>
             <hr />
             <h1 className="h1-green">{props.msg}</h1>
+            <hr />
+            {
+                isTrue && 
+                    <Fragment>
+                        <p>The current value of isTrue is true</p>
+                        <hr />
+                    </Fragment>
+            }
+            <hr />
+            {
+                isTrue
+                ? <p>Is true</p>
+                : <p>Is false</p>
+            }
+            <hr />
+            <a href="#!" className="btn btn-secondary" onClick={toggleTrue}>Toggle isTrue</a>
         </Fragment>
     );
 }
--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppClass from './AppClass';
 import HelloWorld from './HelloWorld';
 
 
@@ -10,7 +9,7 @@ root.render(
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          <AppClass msg="Hello, World!" />
+        
           <HelloWorld msg="hello, world!"/>
         </div>
       </div>
