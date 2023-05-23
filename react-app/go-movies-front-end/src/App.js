ac04b7bd32a87b97aff7e225b616d77f60265c59
Vinesh tk <vineshtk10@gmail.com>
2023-05-23 22:30:18 +0530

    basic staring

--- react-app/go-movies-front-end/src/App.js
+++ react-app/go-movies-front-end/src/App.js
@@ -1,9 +1,32 @@
 
 function App() {
   return (
- <div>
-  go watch a movie
- </div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!"><span className="badge bg-success">Login</span></a>
+        </div>
+        <hr className="mb-4"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">Home</a>
+              <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+
+        </div>
+      </div>
+
+    </div>
   );
 }
 
