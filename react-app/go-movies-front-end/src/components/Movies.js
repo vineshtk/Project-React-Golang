41b2dc5468d3e9ee5240bda4abf02eca2119d0b0
Vinesh tk <vineshtk10@gmail.com>
2023-05-23 23:26:19 +0530

    added ticket image

---
+++ react-app/go-movies-front-end/src/components/Movies.js
@@ -0,0 +1,15 @@
+
+const Movies = () => {
+    return (
+        <>
+            <div className="text-center">
+                <h2>Movies</h2>
+                <hr />
+              
+            </div>
+
+        </>
+    )
+}
+
+export default Movies;
