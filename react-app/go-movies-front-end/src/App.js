ee1b8bb31256cfe10a9713ab73b2dbc414a07eea
Vinesh tk <vineshtk10@gmail.com>
2023-05-23 23:25:47 +0530

    added more items in list

--- react-app/go-movies-front-end/src/App.js
+++ react-app/go-movies-front-end/src/App.js
@@ -1,4 +1,4 @@
-
+import Home from "./components/Home"
 function App() {
   return (
     <div className="container">
@@ -18,11 +18,15 @@ function App() {
             <div className="list-group">
               <a href="#!" className="list-group-item list-group-item-action">Home</a>
               <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+              <a href="#!" className="list-group-item list-group-item-action">Genres</a>
+              <a href="#!" className="list-group-item list-group-item-action">Add Movie</a>
+              <a href="#!" className="list-group-item list-group-item-action">Manage Catalogue</a>
+              <a href="#!" className="list-group-item list-group-item-action">GraphQL</a>
             </div>
           </nav>
         </div>
         <div className="col-md-10">
-
+          <Home />
         </div>
       </div>
 
