a80f732559c507fb9643d9c0e27210cbc5929bc9
Vinesh tk <vineshtk10@gmail.com>
2023-05-24 22:27:06 +0530

    added route for movies

--- react-app/go-movies-front-end/src/index.js
+++ react-app/go-movies-front-end/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Movies from './components/Movies';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children:[
+      {index:true, element:<Home />},
+      {
+        path:"/movies",
+        element:<Movies />
+      }
+    ]
   }
 ])
 
