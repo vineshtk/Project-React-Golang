8184ba3f2f01884534bd2074f3c71568a85a6c05
Vinesh tk <vineshtk10@gmail.com>
2023-05-26 23:08:27 +0530

    added login input element

--- react-app/go-movies-front-end/src/components/Login.js
+++ react-app/go-movies-front-end/src/components/Login.js
@@ -1,13 +1,47 @@
+import { useState } from "react";
+import Input from "./form/Input";
 
 const Login = () => {
-    return (
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-        <div>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <div className="col-md-6 offset-md-3">
             <h2>Login</h2>
             <hr />
 
-        </div>
+            <form onSubmit={handleSubmit}>
 
+                <Input
+                    title="Email Adderess"
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    autoComplete="email-new"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <Input
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    autoComplete="password-new"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+
+                <hr />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                ></input>
+
+            </form>
+        </div>
     )
 }
 
